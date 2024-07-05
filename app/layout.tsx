84fd1448097dc638f4a1ea4572fd5610c51e4c34
Rainer Nsa <woldreamz@gmail.com>
2024-07-05 12:25:35 +0100

    City of God

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,17 @@ import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
-const ibmPlexSerif =  IBM_Plex_Serif({
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-ibm-plex-serif",
-})
+});
 
 export const metadata: Metadata = {
   title: "Krypto",
-  description: "Krypto is a modern banking platform that provides a variety of features that can be used to interact with your financial applications",
+  description:
+    "Krypto is a modern banking platform that provides a variety of features that can be used to interact with your financial applications",
 };
 
 export default function RootLayout({
@@ -21,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" type="image/svg+xml" href="/public/Rainer.png" />
       <body className={inter.className}>{children}</body>
     </html>
   );
