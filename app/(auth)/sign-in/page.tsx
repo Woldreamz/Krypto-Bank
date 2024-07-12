65e53c3f936d17929869a51f4aa6f832e48e6743
Rainer Nsa <woldreamz@gmail.com>
2024-07-12 18:30:18 +0100

    implement initial file and folder structure

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const SignIn = () => {
-  return <div>SignIn price</div>;
+  return (
+    <div>
+      <div>
+        <div>They not like us</div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
