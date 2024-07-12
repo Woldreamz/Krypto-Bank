7ebe0b2e3764ffe3c9f519f44c4bfffdbf2fc52c
Rainer Nsa <woldreamz@gmail.com>
2024-07-12 20:11:31 +0100

    City of God

---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const data = {
+    datasets: [
+      {
+        label: "Banks",
+        data: [150350, 245650, 565000],
+        backgroundColor: ["#0747b6", "#2265d8", "#2f91fa"],
+      },
+    ],
+    labels: ["Bank 1", "Bank 2", "Bank 3"],
+  };
+
+  return <Doughnut data={data} />;
+};
+
+export default DoughnutChart;
