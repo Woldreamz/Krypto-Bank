cbee362449ec882789f277ddb8e5847cd514d280
Rainer Nsa <woldreamz@gmail.com>
2024-07-13 10:03:39 +0100

    City of God

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Rainer" };
+  const loggedIn = {
+    firstName: "Rainer",
+    lastName: "Nsa",
+    email: "woldreamz@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -22,7 +27,14 @@ const Home = () => {
             totalCurrentBalance={150350.45}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[]} // fetch banks and accounts here
+      />
     </section>
   );
 };
