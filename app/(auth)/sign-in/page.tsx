8cc3c33c34bdf5c7851e9e7faf2fc8f8b503d1a1
Rainer Nsa <woldreamz@gmail.com>
2024-07-14 05:55:40 +0100

    City of God

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,12 +1,11 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignIn = () => {
   return (
-    <div>
-      <div>
-        <div>They not like us</div>
-      </div>
-    </div>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
   );
 };
 
