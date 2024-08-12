ae5fdbd0fd9a08bb675bc1d7d10de977cf2a3998
Rainer Nsa <woldreamz@gmail.com>
2024-08-12 13:43:33 +0100

    City of God

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,20 +15,22 @@ const Footer = ({ user, type = "desktop" }: FooterProps) => {
   return (
     <footer className="footer">
       <div className={type === "mobile" ? "footer_name-mobile" : "footer_name"}>
-        <p className="text-xl font-bold text-gray-700">{user.name[0]}</p>
+        <p className="text-xl font-bold text-gray-700">{user?.firstName[0]}</p>
       </div>
 
-      <div className={type === "mobile" ? "footer_email-mobile" : "email"}>
-        <h1 className="text-14 truncate font-semibold text-gray-700">
-          {user.name}
+      <div
+        className={type === "mobile" ? "footer_email-mobile" : "footer_email"}
+      >
+        <h1 className="text-14 truncate text-gray-700 font-semibold">
+          {user?.firstName}
         </h1>
         <p className="text-14 truncate font-normal text-gray-600">
-          {user.email}
+          {user?.email}
         </p>
       </div>
 
       <div className="footer_image" onClick={handleLogOut}>
-        <Image src="icons/logout.svg" fill alt="ray" />
+        <Image src="icons/logout.svg" fill alt="jsm" />
       </div>
     </footer>
   );
